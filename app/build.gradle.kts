9424b458e217126d49660575ff9c1f2b1b724bce
davidalejandroruizdiego <davidalejandroruizdiego@gmail.com>
2025-12-01 19:32:29 -0600

    dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
